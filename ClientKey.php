5d381c3cfd9df243157b2e8b40eca996f9078085
mdmunir <misbahuldmunir@gmail.com>
2014-05-31 16:40:23 +0700

    klient key

--- ClientKey.php
+++ ClientKey.php
@@ -2,69 +2,99 @@
 
 namespace mdm\tools;
 
-use yii\web\Application;
 use yii\web\Cookie;
 use Yii;
 
 /**
  * Description of ClientKey
  *
- * @author MDMunir
+ * @property string $clientUniqueid
+ * 
+ * @author MDMunir <misbahuldmunir@gmail.com>
  */
 class ClientKey extends \yii\base\Behavior
 {
-
     const COOKIE_KEY = '_client_uniqueid';
-    const CACHE_KEY = 'client_properties';
 
-    public $clientUniqueid;
+    private $_clientUniqueid;
+
     public $expire = 31536000; // default 1 year
 
-    public function events()
+    public function init()
+    {
+        parent::init();
+        $cookie = Yii::$app->getRequest()->cookies->get(self::COOKIE_KEY);
+        if ($cookie) {
+            $this->_clientUniqueid = $cookie->value;
+        } else {
+            $str = microtime(true);
+            if (($session = Yii::$app->getSession()) !== null) {
+                $str .= $session->id;
+            }
+            $this->_clientUniqueid = md5($str . ':' . microtime(true));
+            $cookie = new Cookie();
+            $cookie->name = self::COOKIE_KEY;
+            $cookie->value = $this->_clientUniqueid;
+        }
+        $cookie->expire = time() + $this->expire;
+        Yii::$app->getResponse()->cookies->add($cookie);
+    }
+
+    public function getClientUniqueid()
+    {
+        return $this->_clientUniqueid;
+    }
+
+    public function canGetProperty($name, $checkVars = true)
+    {
+        return stripos($name, 'client') === 0 || parent::canGetProperty($name, $checkVars);
+    }
+
+    public function canSetProperty($name, $checkVars = true)
+    {
+        return stripos($name, 'client') === 0 || parent::canSetProperty($name, $checkVars);
+    }
+
+    public function __get($name)
+    {
+        if (stripos($name, 'client') === 0 && strcasecmp($name, 'clientUniqueid')!=0) {
+            return $this->getClientProperty(strtolower($name));
+        } else {
+            return parent::__get($name);
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        if (stripos($name, 'client') === 0 && strcasecmp($name, 'clientUniqueid')!=0) {
+            $this->setClientProperty(strtolower($name), $value);
+        } else {
+            return parent::__get($name);
+        }
+    }
+
+    private function buildKey($key)
     {
         return [
-            Application::EVENT_BEFORE_REQUEST => 'beforeRequest',
+            __CLASS__,
+            $this->_clientUniqueid,
+            $key
         ];
     }
 
-    public function beforeRequest($event)
+    private function setClientProperty($key, $value)
     {
-        $cookie = Yii::$app->request->cookies->get(self::COOKIE_KEY);
-        if ($cookie) {
-            $this->clientUniqueid = $cookie->value;
-        } else {
-            $str = microtime(true);
-            if (($session = Yii::$app->session) !== null) {
-                $str .= $session->id;
-            }
-            $this->clientUniqueid = md5($str);
-            $cookie = new Cookie();
-            $cookie->name = self::COOKIE_KEY;
-            $cookie->value = $this->clientUniqueid;
-        }
-        $cookie->expire = time() + $this->expire;
-        Yii::$app->response->cookies->add($cookie);
-    }
-
-    public function setClientProperties($key, $value)
-    {
-        if ($this->clientUniqueid === null) {
-            throw new \yii\base\UserException('Sabar gan....');
-        }
-        if (($cache = Yii::$app->cache) !== null) {
-            $cache->set([self::CACHE_KEY, $this->clientUniqueid, $key], $value);
+        if (($cache=  Yii::$app->cache) !== null) {
+            $cache->set($this->buildKey($key), $value);
         }
     }
 
-    public function getClientProperties($key)
+    private function getClientProperty($key)
     {
-        if ($this->clientUniqueid === null) {
-            throw new \yii\base\UserException('Sabar gan....');
+        if (($cache=  Yii::$app->cache) !== null) {
+            $result = $cache->get($this->buildKey($key));
+            return $result === false ? null : $result;
         }
-        if (($cache = Yii::$app->cache) !== null) {
-            return $cache->get([self::CACHE_KEY, $this->clientUniqueid, $key]);
-        }
-        return false;
+        return null;
     }
-
 }
